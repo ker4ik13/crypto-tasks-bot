d8bdff14d46cb90634d9becd83435f89adae7474
ker4ik13 <kekustop228@mail.ru>
2024-10-26 00:30:54 +0300

    fix: beautyCurrency.ts 3 digits after dot

--- src/lib/helpers/beautyCurrency.ts
+++ src/lib/helpers/beautyCurrency.ts
@@ -1,3 +1,9 @@
 export const beautyCurrency = (amount: number, numbersOfDigits = 2) => {
+  const numberToString = amount.toString();
+
+  if (numberToString.length >= 5) {
+    return amount.toFixed(3);
+  }
+
   return amount.toFixed(numbersOfDigits);
 };
